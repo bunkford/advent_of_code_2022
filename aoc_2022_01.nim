4098fb3cd719b33a830c9b52d60cab0e4043b4be
Duncan Clarke <bunkford@gmail.com>
2022-12-01 01:08:40 -0500

    Day 1 - Part 2

--- aoc_2022_01.nim
+++ aoc_2022_01.nim
@@ -1,12 +1,17 @@
-import os, strutils
+import os, strutils, algorithm
 
 var total:int
 var highest:int
+var totals:seq[int]
 for line in (getAppDir() / "aoc_2022_01.txt").lines:
   if line == "": 
+    totals.add(total)
     if total > highest: highest = total
     total = 0
   else:
     total.inc line.parseInt
 
+totals.sort()
+
 echo "Answer Part 1: ", highest
+echo "Answer Part 2: ", totals[^1] + totals[^2] + totals[^3]
